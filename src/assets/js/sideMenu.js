a31d7513f87da8e2327b6b9658f3c4f2e23ec733
RoseDeLaAnne <mwexcellency@outlook.com>
2023-06-18 23:14:22 +0300

    1.5.2

---
+++ src/assets/js/sideMenu.js
@@ -0,0 +1,32 @@
+const openUl = (name, level) => {
+    var span = document.getElementById(`side-menu-${name}-ul-${level}-span`);
+    var svg = document.getElementById(`side-menu-${name}-ul-${level}-svg`);
+    var svgId = document.getElementById(`side-menu-${name}-ul-${level}-svg-id`);
+    var svgIdIconName = svgId.href.baseVal.slice(
+        svgId.href.baseVal.lastIndexOf("#") + 1
+    );
+    var ul = document.getElementById(`side-menu-${name}-ul-${level}`);
+
+    if (level === 1) {
+        span.classList.toggle("text-gray");
+        svg.classList.toggle("stroke-textPrimary");
+        svg.classList.toggle("stroke-stroke-1");
+    } else if (level === 2) {
+        span.classList.toggle("text-textPrimary");
+        span.classList.toggle("text-accent-2");
+        svg.classList.toggle("fill-textPrimary");
+        svg.classList.toggle("fill-gray");
+    }
+    if (svgIdIconName === "plus") {
+        svgId.setAttribute("href", "assets/icons/sprites.svg#minus");
+    } else if (svgIdIconName === "minus") {
+        svgId.setAttribute("href", "assets/icons/sprites.svg#plus");
+    } else if (svgIdIconName === "arrow_top") {
+        svgId.setAttribute("href", "assets/icons/sprites.svg#arrow_bottom");
+    } else if (svgIdIconName === "arrow_bottom") {
+        svgId.setAttribute("href", "assets/icons/sprites.svg#arrow_top");
+    }
+
+    ul.classList.toggle("max-h-0");
+    ul.classList.toggle("max-h-96");
+};
---
+++ src/assets/js/sortBy.js
@@ -0,0 +1,28 @@
+var sortByWindow = document.getElementById("sort-by-window");
+var sortBySvgId = document.getElementById("sort-by-svg-id");
+var sortBySvgIdIconName = sortBySvgId.href.baseVal.slice(
+    sortBySvgId.href.baseVal.lastIndexOf("#") + 1
+);
+
+const openSortByWindow = () => {
+    if (sortBySvgIdIconName === "arrow_bottom") {
+        sortBySvgId.setAttribute("href", "assets/icons/sprites.svg#arrow_top");
+    } else {
+        sortBySvgId.setAttribute(
+            "href",
+            "assets/icons/sprites.svg#arrow_bottom"
+        );
+    }
+
+    sortByWindow.classList.toggle("visible");
+    sortByWindow.classList.toggle("invisible");
+    sortByWindow.classList.toggle("opacity-0");
+    sortByWindow.classList.toggle("opacity-100");
+};
+
+const selectLabelSortBy = () => {
+    sortByWindow.classList.remove('opacity-100')
+    sortByWindow.classList.remove('visible')
+    sortByWindow.classList.add('opacity-0')
+    sortByWindow.classList.add('invisible')
+};
