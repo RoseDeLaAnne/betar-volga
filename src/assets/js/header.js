b48a217b261efba1efdff0aa297c6e096059b01b
RoseDeLaAnne <mwexcellency@outlook.com>
2023-06-26 20:37:19 +0300

    1.7.5

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -8,23 +8,36 @@ var scrollThreshold = 100; // Adjust this value to your desired scroll threshold
 var scrollDirection = "down";
 var lastScrollPosition = 0;
 
-const cartSide = document.getElementById('cart-side')
-const cartSideBlock = document.getElementById('cart-side-block')
+const cartSide = document.getElementById("cart-side");
+const cartSideBlock = document.getElementById("cart-side-block");
 
-const catalogPopUpBlock = document.getElementById('catalog-pop-up-block')
+const catalogPopUpBlock = document.getElementById("catalog-pop-up-block");
 
 function toggleFixedHeader() {
-    console.log(headerHeight)
 
     scrollPosition = window.scrollY || document.documentElement.scrollTop;
-
-    cartSide.classList.add(`h-[calc(100%-${headerHeight}px)]`)
-    cartSideBlock.classList.add(`h-[calc(100%-${headerHeight}px)]`)
-    cartSide.classList.add(`top-[${headerHeight}px]`)
 
     if (scrollPosition > lastScrollPosition) {
         // Scrolling down
         scrollDirection = "down";
+
+        if (window.innerWidth <= 1024) {
+            cartSide.classList.remove(`top-[92px]`);
+            cartSide.classList.remove(`h-[calc(100%-92px)]`);
+            cartSideBlock.classList.remove(`h-[calc(100%-92px)]`);
+
+            cartSide.classList.add(`top-[104px]`);
+            cartSide.classList.add(`h-[calc(100%-104px)]`);
+            cartSideBlock.classList.add(`h-[calc(100%-104px)]`);
+        } else {
+            cartSide.classList.remove("top-[104px]");
+            cartSide.classList.remove("lg:top-[220px]");
+            cartSideBlock.classList.remove("lg:top-[220px]");
+
+            cartSide.classList.add(`top-[92px]`);
+            cartSide.classList.add(`h-[calc(100%-92px)]`);
+            cartSideBlock.classList.add(`h-[calc(100%-92px)]`);
+        }
 
         if (scrollPosition > 48) {
             header.classList.add("fixed");
@@ -36,34 +49,36 @@ function toggleFixedHeader() {
             headerLevel3.classList.remove("max-h-64");
             headerLevel3.classList.add("max-h-0");
 
-            // cartSide.classList.add(`h-[calc(100%-${headerHeight}px)]`)
-            // cartSide.classList.remove('h-[calc(100%-220px)]')
-            // cartSideBlock.classList.add(`h-[calc(100%-${headerHeight}px)]`)
-            // cartSideBlock.classList.remove('h-[calc(100%-220px)]')
-            // cartSide.classList.add(`top-[${headerHeight}px]`)
-            // cartSide.classList.remove('top-[220px]')
-
-            // catalogPopUpBlock.classList.remove('top-[220px]')
-            catalogPopUpBlock.classList.add(`top-[${headerHeight}px]`)
+            catalogPopUpBlock.classList.add(`top-[${headerHeight}px]`);
         }
     } else {
         // Scrolling up
         scrollDirection = "up";
+
+        if (window.innerWidth <= 1024) {
+            cartSide.classList.remove(`top-[92px]`);
+            cartSide.classList.remove(`h-[calc(100%-92px)]`);
+            cartSideBlock.classList.remove(`h-[calc(100%-92px)]`);
+
+            cartSide.classList.add(`top-[104px]`);
+            cartSide.classList.add(`h-[calc(100%-104px)]`);
+            cartSideBlock.classList.add(`h-[calc(100%-104px)]`);
+        } else {
+            cartSide.classList.remove(`top-[92px]`);
+            cartSide.classList.remove(`h-[calc(100%-92px)]`);
+            cartSideBlock.classList.remove(`h-[calc(100%-92px)]`);
+
+            cartSide.classList.add(`top-[220px]`);
+            cartSide.classList.add(`h-[calc(100%-220px)]`);
+            cartSideBlock.classList.add(`h-[calc(100%-220px)]`);
+        }
 
         headerLevel1.classList.remove("max-h-0");
         headerLevel1.classList.add("max-h-64");
         headerLevel3.classList.remove("max-h-0");
         headerLevel3.classList.add("max-h-64");
 
-        // cartSide.classList.remove(`h-[calc(100%-${headerHeight}px)]`)
-        // cartSide.classList.add('h-[calc(100%-220px)]')
-        // cartSideBlock.classList.remove(`h-[calc(100%-${headerHeight}px)]`)
-        // cartSideBlock.classList.add('h-[calc(100%-220px)]')
-        // cartSide.classList.remove(`top-[${headerHeight}px]`)
-        // cartSide.classList.add('top-[220px]')
-
-        catalogPopUpBlock.classList.remove(`top-[${headerHeight}px]`)
-        // catalogPopUpBlock.classList.add('top-[220px]')
+        catalogPopUpBlock.classList.remove(`top-[${headerHeight}px]`);
     }
 
     lastScrollPosition = scrollPosition;
